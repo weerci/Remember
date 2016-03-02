5e5f3dc2fea1358427105cdde924a62bfa98107a
dima <weerci@yandex.ru>
2016-03-02 16:40:51 +0700

    0005

--- app/src/main/java/com/ortosoft/remember/db/members/Group.java
+++ app/src/main/java/com/ortosoft/remember/db/members/Group.java
@@ -10,46 +10,99 @@ import java.util.ArrayList;
 
 /**
  * Created by dima on 29.02.2016.
+ * Класс реализует функционал групп объединяющих людей (семья, работа и пр.)
  */
 public class Group {
 
     // region Fields
 
-    private int id = -1;
-    private  String name;
+    private int _id = -1;
+    private  String _name;
+    private ArrayList<Member> _members;
 
     // endregion
-    public Group(String name) {
-        this.name = name;
+
+    public Group(String _name) {
+        this._name = _name;
     }
 
     // region Properties
 
-    public int getId() {
-        return id;
+    // Возвращает _id записи таблицы базы данных
+    public int get_id() {
+        return _id;
     }
 
-    public String getName() {
-        return name;
+    // Возвращает наименование группы
+    public String get_name() {
+        return _name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    // Устанавливает наименование группы
+    public void set_name(String _name) {
+        this._name = _name;
+    }
+
+    // Возвращает список людей входящих в группу
+    public ArrayList<Member> get_members() {
+        return _members;
     }
 
     // endregion
 
-    // Сохраняет новый или измененный объект в базе данных
+    // Сохраняет новую группу в базе или изменяет название новой
     public long Save(Context context){
-        return (id == -1) ? insert(getName(), context) : update(getName(), context);
+        return (_id == -1) ? insert(context) : update(context);
     }
 
-    // region Insert, Update, Delete
+    // Добавляет человека в группу
+    public void AddMember(Member member, Context context)
+    {
+        insertMember(member, context);
+        refresh();
+    }
 
-    private long insert(String name, Context context)
+    // Удаляет человека из группы
+    public void DelMember(Member member)
+    {
+        _members.remove(member);
+    }
+
+    // region Insert, Update, Delete, Refresh
+
+    // Обновляет данные о группе
+    private void refresh()
+    {
+        Group group = FindById(this._id);
+        this._id = group.get_id();
+        this._name = group.get_name();
+        this._members = group.get_members();
+    }
+
+    private long insertMember(Member member, Context context)
+    {
+//        ContentValues cv = new ContentValues();
+//        cv.put(Tables.GroupMember.COLUMN_NAME, );
+//
+//        Connect connect = Connect.Item(context);
+//        connect.getDb().beginTransaction();
+//
+//        long result = 0;
+//        try {
+//            result = connect.getDb().insert(Tables.GroupMember.TABLE_NAME, null, cv);
+//            connect.getDb().setTransactionSuccessful();
+//        } finally {
+//            connect.getDb().endTransaction();
+//        }
+
+//        return result;
+        return 0;
+    }
+
+    private long insert(Context context)
     {
         ContentValues cv = new ContentValues();
-        cv.put(Tables.TableGroup.COLUMN_NAME, name);
+        cv.put(Tables.TableGroup.COLUMN_NAME, this._name);
 
         Connect connect = Connect.Item(context);
         connect.getDb().beginTransaction();
@@ -64,12 +117,24 @@ public class Group {
 
         return result;
     }
-    private long update(String name, Context context)
+    private long update(Context context)
     {
-        return 1;
+        ContentValues cv=new ContentValues();
+        cv.put(Tables.TableGroup.COLUMN_NAME, this._name);
+
+        Connect connect = Connect.Item(context);
+        connect.getDb().beginTransaction();
+        int result;
+        try {
+            result = connect.getDb().update(Tables.TableGroup.TABLE_NAME, cv, Tables.TableGroup.COLUMN_ID + " = ?", new String[] { String.valueOf(this._id) });
+            connect.getDb().setTransactionSuccessful();
+        } finally {
+            connect.getDb().endTransaction();
+        }
+        return result;
     }
 
-    // Удаление группы по ее id
+    // Удаление группы по ее _id
     public static void Delete(int id, Context context)
     {
         return;
